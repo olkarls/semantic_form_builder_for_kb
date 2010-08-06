5e5a1e70c91252db8e6d1d4e8a1fe01cf244f7b9
Ola Karlsson <olkarls@gmail.com>
2010-08-06 15:41:17 +0200

    Copied old files

---
+++ init.rb
@@ -0,0 +1,16 @@
+# load_paths.each do |path|
+#   ActiveSupport::Dependencies.load_once_paths.delete(path)
+# end if Rails.development?
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+  if html_tag =~ /type="hidden"/ || html_tag =~ /<label/
+    html_tag
+  else
+    "<span class=\"error_message\">#{[instance_tag.error_message].flatten.first}</span>"
+    "#{html_tag}"
+  end
+end
+
+config.to_prepare do
+  ApplicationController.helper(FormHelper)
+end
